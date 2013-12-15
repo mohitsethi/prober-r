06e7d8a59cecafc31d0bf2daef105c7b230e1276
mohitsethi <mohit@sethis.in>
2013-12-15 14:15:08 +0530

    app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProberR::Application.config.secret_key_base = 'f6e6f9421333652724d88d48fb21c44a81d87e365072056aec25a618c434eedc91643709001371d82147348c45f5a1a6882710c2d06d0595f3633a3dcb53f57b'
